6115a2bf56c27456ba2e66b4e5cb7a828a0ea2d0
Kaitsuru <aluizio@dcc.ufrj.br>
2015-07-22 16:19:31 -0300

    Refactory the code.

--- community/models.py
+++ community/models.py
@@ -5,17 +5,11 @@ from geo1 import settings
 class Community(models.Model):
 
     name = models.CharField(max_length = 150)
-
     description = models.TextField(null=True, blank=True)
-
     need_invitation = models.BooleanField(default=False)
-
     date_creation = models.DateTimeField(default=datetime.datetime.now(), null=True)
-
     members = models.ManyToManyField(settings.AUTH_USER_MODEL, through='MembershipCommunity', related_name='communities')
-
     manager = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='manager_of_community', db_column='id_manager')
-
 
     def users(self):
         return self.members.all()
@@ -41,29 +35,19 @@ class Community(models.Model):
 class MembershipCommunity(models.Model):
 
     member = models.ForeignKey(settings.AUTH_USER_MODEL)
-
     community = models.ForeignKey(Community)
-
     date_joined = models.DateField(default=datetime.datetime.now())
-
     is_blocked = models.BooleanField(default=False)
-
     is_banned = models.BooleanField(default=False)
-
     invite_reason = models.CharField(max_length=100, null=True, default='Joined us')
 
     def is_included(self, a_person, a_community):
-
         return (self.objects.get(member=a_person, community=a_community)) is not None
 
     def is_not_included(self, a_person, a_community):
-
         return not self.is_person_included_in_community(self, a_person, a_community)
 
     def join_us(self, a_person, a_community, an_invited_reason):
-
         if self.is_not_included(a_person, a_community):
-
             return self.objects.create(member=a_person,community=a_community, invited_reason=an_invited_reason)
-
         return None
--- community_app/static/community_app/editor.js
+++ community_app/static/community_app/editor.js
@@ -1,168 +1,297 @@
-/**
- * Created by Rogério on 12/06/2015.
- */
-    <script src="http://cdn.leafletjs.com/leaflet-0.7/leaflet.js">
-    </script>
-    <script src="http://leaflet.github.io/Leaflet.draw/leaflet.draw.js">
-    </script>
+$("input").addClass("form-control");
 
-   <script>
-        var map = L.map('map', initializeContextMenuMap()).setView([-21.2858, -41.78682], 9);
-        var geojsonlayer = null;
-        var jsons_div = document.getElementById("json_div");
-        var editableGeojson = null;
+var map = L.map('map', initializeContextMenuMap()).setView([-21.2858, -41.78682], 2);
+var editableGeojson = null;
+var json_properties = [];
+var schema_community_information_array = JSON.parse($schema.text());
+var empty_properties = $sjs.text();
+var actuallayer = null;
 
-        mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18
-            }).addTo(map);
+mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18
+    }).addTo(map);
 
-        function clickOnLayer(feature, layer ){
+function clickOnLayer(feature, layer ){
+    console.log(layer);
+    actuallayer = layer;
+}
+
+function dragEndLayer(feature, layer ){
+    console.log(layer.getLatLng());
+}
+
+function showCoordinates (e) {
+    console.log(e.latlng);
+}
+
+function centerMap (e) {
+    map.panTo(e.latlng);
+}
+
+function zoomIn (e) {
+    map.zoomIn();
+}
+
+function zoomOut (e) {
+    map.zoomOut();
+}
+
+function populateFeatureWhithModal() {
+    aFeature = actuallayer.feature;
+    propers = JSON.parse(aFeature.properties.properties);
+    for (i = 0; i < schema_community_information_array.length; i++) {
+
+        field_name = schema_community_information_array[i];
+        field_html = document.getElementById('id_' + field_name);
+        propers[field_name]= field_html.value;
+
+    }
+    actuallayer.feature.properties.properties = JSON.stringify(propers);
+}
+
+function featureIsNew(feature){
+    return feature.id == null;
+}
+
+function saveGeometry(){
+    populateFeatureWhithModal();
+    var content = actuallayer.feature;
+
+    var url = "";
+    if(featureIsNew(content)) {
+        url = url_create;
+    }
+    else {
+        url = url_update+content.id;
+    }
+
+    $.post(url,
+        {
+            _content_type: "application/json",
+            _content: dataJson
+        },
+        function (data) {
+            console.log(data);
+        }
+    );
+}
+
+function editingAttributes(layer){
+    populateModalWithFeature(layer);
+    $('#myModal').modal('show');
+}
+
+function populateModalWithFeature(layer) {
+    actuallayer = layer;
+    aFeature = layer.feature;
+
+    for (i = 0; i < schema_community_information_array.length; i++) {
+        field_name = schema_community_information_array[i];
+        field_value = (JSON.parse(aFeature.properties.properties))[field_name];
+        field_html = document.getElementById('id_' + field_name);
+        field_html.value=field_value;
+    }
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+function initializeContextMenuMap() {
+    return {
+        zoom: 15,
+        contextmenu: true,
+        contextmenuWidth: 140,
+        contextmenuItems: [{
+            text: 'Show coordinates',
+            callback: showCoordinates
+        }, {
+            text: 'Center map here',
+            callback: centerMap
+        }, '-', {
+            text: 'Zoom in',
+            callback: zoomIn
+        }, {
+            text: 'Zoom out',
+            callback: zoomOut
+        }]
+    }
+}
+
+function onEachFeature(feature, layer) {
+    layer.options.draggable = true;
+    layer.on('click', function() { clickOnLayer(feature, layer); });
+    layer.on('dragend', function() { dragEndLayer(feature, layer)});
+    binderMenuContextTo(layer);
+    json_properties.push(feature.properties);
+}
+function binderMenuContextTo(layer) {
+    layer.bindContextMenu({
+        contextmenu: true,
+        contextmenuInheritItems: true,
+        contextmenuItems: [
+            {
+                text: 'Marker ',
+                callback: function () { alert('Marker: ' + layer.feature.id);      }
+            }, {
+                text: 'Edit attributes',
+                callback: function () { editingAttributes(layer);  }
+            }, {
+                separator: true
+            }]
+    });
+
+}
+function pointToLayer(data, latLng) {
+    if (data.geometry.type != 'Point')
+        return;
+    var marker;
+    marker = new L.Marker(latLng, {
+          title: data.id,
+          contextmenu: true,
+          contextmenuItems: [{
+              text: 'Marker item',
+              index: 0,
+              callback: function () {
+                  alert('Marker: ' + marker.feature.id);
+              }
+          }, {
+              text: 'Edit attributes',
+              index: 1,
+              callback: function () { editingAttributes(data); }
+          },
+              {
+              separator: true,
+              index: 1
+          }]
+    });
+    return marker;
+}
+
+function initializeEditableGeoJson(geoJsons) {
+    editableGeojson = L.geoJson(geoJsons,  {onEachFeature: onEachFeature});
+
+    map.addLayer(editableGeojson);
+    options = function(editableGeojson) {
+        return {
+            position: 'topleft',
+            draw: {
+
+                rectangle: true,
+                polygon: true,
+                polyline: true,
+                circle: true,
+                marker: true
+            },
+            edit: {
+                featureGroup: editableGeojson, //REQUIRED!!
+                remove: true
+            }
+        };
+    };
+    var drawControl = new L.Control.Draw(options(editableGeojson));
+    map.addControl(drawControl);
+
+    map.on('draw:created', function (e) {
+        var type = e.layerType,
+            layer = e.layer,
+            a_feature = layer.toGeoJSON();
+        a_feature.id = null;
+        console.log(a_feature);
+        a_feature.properties.properties = empty_properties;
+        layer.feature = a_feature;
+        editableGeojson.addLayer( layer);
+        actuallayer = layer;
+
+        if (type === 'marker') {
+            // Do marker specific actions
+            console.log(type);
+        }
+
+        // Do whatever else you need to. (save to db, add to map etc)
+        map.addLayer(layer);
+        binderMenuContextTo(layer);
+    });
+
+    map.on('draw:edited', function (e) {
+        var layers = e.layers;
+            layers.eachLayer(function (layer) {
+                //do whatever you want, most likely save back to db
+                console.log('entrei');
+                console.log(layer);
+            });
+    });
+
+    map.on("draw:deleted", function(e){
+        var layers = e.layers;
+        layers.eachLayer(function (layer) {
+            //do whatever you want, most likely save back to db
+            console.log('entrei_no delete');
             console.log(layer);
-        }
-        function dragEndLayer(feature, layer ){
-            console.log(layer.getLatLng());
-        }
-
-         function showCoordinates (e) {
-	        console.log(e.latlng);
-         }
-
-        function centerMap (e) {
-	      map.panTo(e.latlng);
-        }
-
-      function zoomIn (e) {
-	      map.zoomIn();
-      }
-
-      function zoomOut (e) {
-	      map.zoomOut();
-      }
-
-      function editingAttributes(feature){
-          console.log(feature);
-      }
-
-      function initializeContextMenuMap() {
-           return {
-              zoom: 15,
-              contextmenu: true,
-              contextmenuWidth: 140,
-              contextmenuItems: [{
-                  text: 'Show coordinates',
-                  callback: showCoordinates
-              }, {
-                  text: 'Center map here',
-                  callback: centerMap
-              }, '-', {
-                  text: 'Zoom in',
-                  callback: zoomIn
-              }, {
-                  text: 'Zoom out',
-                  callback: zoomOut
-              }]
-          }
-      }
-
-    </script>
-    <script>
-        function onEachFeature(feature, layer) {
-            layer.options.draggable = true;
-            layer.bindContextMenu({
-                contextmenu: true,
-                contextmenuInheritItems: true,
-                contextmenuItems: [
-                    {
-                        text: 'Marker ',
-                        callback: function () { alert('Marker: ' + layer.feature.id);      }
-                    }, {
-                        text: 'Edit attributes',
-                        callback: function () { editingAttributes(feature);  }
-                    }, {
-                        separator: true
-
-                    }]
+            var id = layer.feature.id;
+            console.log(id);
+            var url = url_update+id;
+            $.ajax({
+                method: "DELETE",
+                url: url
             });
-            layer.on('click', function() { clickOnLayer(feature, layer); });
-            layer.on('dragend', function() { dragEndLayer(feature, layer)});
-
-        }
-        function pointToLayer(data, latLng) {
-            if (data.geometry.type != 'Point')
-                return;
-            var marker;
-            marker = new L.Marker(latLng, {
-                  title: data.id,
-                  contextmenu: true,
-                  contextmenuItems: [{
-                      text: 'Marker item',
-                      index: 0,
-                      callback: function () {
-                          alert('Marker: ' + marker.feature.id);
-                      }
-                  }, {
-                      text: 'Edit attributes',
-                      index: 1,
-                      callback: function () { editingAttributes(data); }
-                  },
-                      {
-                      separator: true,
-                      index: 1
-                  }]
-            });
-            return marker;
-        }
-    </script>
-    <script>
-        var geojsonMarkerOptions = {
-            radius: 8,
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 0.8,
-            fillOpacity: 0.1,
-            draggable: true
-        };
-        function initializeEditableGeoJson(geoJsons) {
-            editableGeojson = L.geoJson(geoJsons,  {onEachFeature: onEachFeature});
-
-            map.addLayer(editableGeojson);
-            options = function(editableGeojson) {
-                return {
-                    position: 'topleft',
-                    draw: {
-
-                        rectangle: true,
-                        polygon: true,
-                        polyline: true,
-                        circle: true,
-                        marker: true
-                    },
-                    edit: {
-                        featureGroup: editableGeojson, //REQUIRED!!
-                        remove: true
-                    }
-                };
-            };
-            var drawControl = new L.Control.Draw(options(editableGeojson));
-            map.addControl(drawControl);
-            map.on('draw:edited', function (e) {
-                var layers = e.layers;
-                    layers.eachLayer(function (layer) {
-                        //do whatever you want, most likely save back to db
-                        console.log('entrei');
-                        console.log(layer);
-                    });
-            });
-        };
-    </script>
-    <script>
-        var jqxhr = $.getJSON( "{{url_json}}", function(geoJsons) {
-
-            initializeEditableGeoJson(geoJsons);
         });
+    });
+};
 
-    </script>
+$.getJSON( url_json, function(geoJsons) {
+        initializeEditableGeoJson(geoJsons);
+    })
+    .done(function() {
+        $.jsontotable(json_properties, { id: '#json_table', header: false });
+    }
+);
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+            // Send the token to same-origin, relative URLs only.
+            // Send the token only if the method warrants CSRF protection
+            // Using the CSRFToken value acquired earlier
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});

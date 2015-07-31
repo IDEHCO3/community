ee1c3da1cb3dba81b86eecc533e867e2193ca192
Kaitsuru <aluizio@dcc.ufrj.br>
2015-07-31 11:00:49 -0300

    Save, update and delete implemented to schema layer.

--- create_layer/static/create_layer/new_layer.js
+++ create_layer/static/create_layer/new_layer.js
@@ -10,6 +10,68 @@ var removeMessages = function(){
     $("#alertMessages").empty();
 };
 
+var getSize = function(options){
+    var obj = JSON.parse(options);
+    return obj['max_length'];
+};
+
+var getDecimal = function(attr){
+    if(attr['type_field'] == 'DecimalField'){
+        return 5;
+    }
+    else
+        return 0;
+};
+
+var getTypeFromRestFul = function(type){
+    switch(type){
+        case 'CharField':
+            type = 'Character';
+            break;
+        case 'DecimalField':
+        case 'IntegerField':
+            type = 'Number';
+            break;
+        case 'BooleanField':
+            type = 'Logical';
+            break;
+        case 'DateTimeField':
+            type = 'Date';
+            break;
+    }
+
+    return type;
+};
+
+var convertToNG = function(attr,i){
+    var newAttr = { 'id': i,
+                    'pk': attr['id'],
+                    'attributeName': attr['name_field'],
+                    'attributeType': getTypeFromRestFul(attr['type_field']),
+                    'attributeSize': getSize(attr['options']),
+                    'attributeDecimal': getDecimal(attr)};
+    return newAttr;
+};
+
+var globalData = [];
+
+var loadData = function(dataIncoming){
+    data = [];
+    for(var i=0; i<dataIncoming.length; i++){
+        var attr = dataIncoming[i];
+        var newAttr = convertToNG(attr,i);
+
+        data.push(newAttr);
+    }
+
+    globalData = data;
+};
+
+var reloadData = function(){
+    $.get(url_filter).done(loadData);
+    return globalData;
+};
+
 (function(){
     var app = angular.module("layer_app",[]);
     var data = [];
@@ -18,70 +80,14 @@ var removeMessages = function(){
 
         $scope.layerName = 'myLayer';
         $scope.layerType = 'Point';
-        $scope.attributes = [{  'id': 0,
-                                'pk': null,
-                                'attributeName': 'id',
-                                'attributeType': 'Number',
-                                'attributeSize': 10,
-                                'attributeDecimal': 0 }];
+        $scope.attributes = reloadData();
 
         $scope.attributeName = 'attributeName';
         $scope.attributeType = 'Character';
         $scope.attributeSize = 50;
         $scope.attributeDecimal = 0;
 
-        var getSize = function(options){
-            var obj = JSON.parse(options);
-            return obj['max_length'];
-        };
-
-        var getDecimal = function(attr){
-            if(attr['type_field'] == 'DecimalField'){
-                return 5;
-            }
-            else
-                return 0;
-        };
-
-        var getTypeFromRestFul = function(type){
-            switch(type){
-                case 'CharField':
-                    type = 'Character';
-                    break;
-                case 'DecimalField':
-                case 'IntegerField':
-                    type = 'Number';
-                    break;
-                case 'BooleanField':
-                    type = 'Logical';
-                    break;
-                case 'DateTimeField':
-                    type = 'Date';
-                    break;
-            }
-
-            return type;
-        };
-
-        var loadData = function(dataIncoming){
-            for(var i=0; i<dataIncoming.length; i++){
-                var attr = dataIncoming[i];
-                var newAttr = { 'id': i,
-                                'pk': attr['id'],
-                                'attributeName': attr['name_field'],
-                                'attributeType': getTypeFromRestFul(attr['type_field']),
-                                'attributeSize': getSize(attr['options']),
-                                'attributeDecimal': getDecimal(attr)};
-
-                data.push(newAttr);
-            }
-        };
-
-        $.get(url_filter).done(loadData);
-
-        if(data.length > 0){
-            $scope.attributes = data;
-        }
+        $scope.deletedList = [];
 
         this.setAttributeType = function(type){
             $scope.attributeType = type;
@@ -107,62 +113,6 @@ var removeMessages = function(){
             else{
                 return false;
             }
-        };
-
-        this.createAttribute = function(){
-
-            if( $scope.attributeName === '' )
-                return;
-
-            if( this.hasSize() && $scope.attributeSize === 0 )
-                return;
-
-            if( $scope.attributeType === 'Date'){
-                $scope.attributeSize = 8;
-            }
-            else if( $scope.attributeType === 'Logical' ){
-                $scope.attributeSize = 1;
-            }
-
-            if( typeof $scope.attributeSize != 'number' )
-                return;
-
-            if( typeof $scope.attributeDecimal != 'number' )
-                return;
-
-            if( $scope.attributeSize > 255 || $scope.attributeSize <= 0 )
-                return;
-
-            if( $scope.attributeDecimal > 255 || $scope.attributeDecimal < 0 )
-                return;
-
-            $scope.attributes.push({'id': $scope.attributes.length,
-                                    'attributeName': $scope.attributeName,
-                                    'attributeType':  $scope.attributeType,
-                                    'attributeSize': $scope.attributeSize,
-                                    'attributeDecimal': $scope.attributeDecimal });
-
-            $scope.attributeName = 'attributeName';
-            $scope.attributeType = 'Character';
-            $scope.attributeSize = 50;
-            $scope.attributeDecimal = 0;
-        };
-
-        $scope.removeAttribute = function(index){
-
-            $scope.attributes.splice(index,1);
-
-            for(var i=0; i < $scope.attributes.length; i++){
-                $scope.attributes[i]['id'] = i;
-            }
-        };
-
-        $scope.editAttribute = function(index){
-            $scope.attributeName = $scope.attributes[index]['attributeName'];
-            $scope.attributeType = $scope.attributes[index]['attributeType'];
-            $scope.attributeSize = $scope.attributes[index]['attributeSize'];
-            $scope.attributeDecimal = $scope.attributes[index]['attributeDecimal'];
-            this.removeAttribute(index);
         };
 
         var convertType = function(type, decimal){
@@ -203,9 +153,106 @@ var removeMessages = function(){
             };
         };
 
+        var saveAttributeOnDB = function(attr){
+            removeMessages();
+            var localData = convertToRestFul(attr);
+            var dataJson = JSON.stringify(localData);
+
+            $.post(url_create,
+                {
+                    _content_type: "application/json",
+                    _content: dataJson
+                }
+            ).done(function (dataIncoming) {
+                    showMessage("Succes to save attribute!", "success");
+
+            }).fail(function(){
+                    showMessage("Error to save attribute!", "danger");
+            });
+        };
+
+        this.createAttribute = function(){
+
+            if( $scope.attributeName === '' )
+                return;
+
+            if( this.hasSize() && $scope.attributeSize === 0 )
+                return;
+
+            if( $scope.attributeType === 'Date'){
+                $scope.attributeSize = 8;
+            }
+            else if( $scope.attributeType === 'Logical' ){
+                $scope.attributeSize = 1;
+            }
+
+            if( typeof $scope.attributeSize != 'number' )
+                return;
+
+            if( typeof $scope.attributeDecimal != 'number' )
+                return;
+
+            if( $scope.attributeSize > 255 || $scope.attributeSize <= 0 )
+                return;
+
+            if( $scope.attributeDecimal > 255 || $scope.attributeDecimal < 0 )
+                return;
+
+            var attr = {'id': $scope.attributes.length,
+                        'pk': null,
+                        'attributeName': $scope.attributeName,
+                        'attributeType':  $scope.attributeType,
+                        'attributeSize': $scope.attributeSize,
+                        'attributeDecimal': $scope.attributeDecimal };
+
+            //saveAttributeOnDB(attr);
+            $scope.attributes.push(attr);
+            //reloadData();
+
+            $scope.attributeName = 'attributeName';
+            $scope.attributeType = 'Character';
+            $scope.attributeSize = 50;
+            $scope.attributeDecimal = 0;
+        };
+
+        var deleteAttributesFromDB = function(){
+            for(var i=0; i<$scope.deletedList.length; i++) {
+                var pk = $scope.deletedList[i];
+                var url_delete = url_update + pk + "/";
+                $.ajax({
+                    url: url_delete,
+                    method: "DELETE"
+                });
+            }
+        };
+
+        $scope.removeAttribute = function(index){
+            var id = $scope.attributes[index]['pk'];
+            if( id != null ) $scope.deletedList.push(id);
+
+            //deleteAttributeFromDB(index);
+            //reloadData();
+            $scope.attributes.splice(index,1);
+
+            for(var i=0; i < $scope.attributes.length; i++){
+                $scope.attributes[i]['id'] = i;
+            }
+        };
+
+        $scope.editAttribute = function(index){
+            $scope.attributeName = $scope.attributes[index]['attributeName'];
+            $scope.attributeType = $scope.attributes[index]['attributeType'];
+            $scope.attributeSize = $scope.attributes[index]['attributeSize'];
+            $scope.attributeDecimal = $scope.attributes[index]['attributeDecimal'];
+            this.removeAttribute(index);
+        };
+
         this.sendData = function(){
             removeMessages();
+            deleteAttributesFromDB();
+
             for(var i=0; i<$scope.attributes.length; i++) {
+                if($scope.attributes[i]['pk'] != null) continue;
 
                 var data = convertToRestFul($scope.attributes[i]);
                 var dataJson = JSON.stringify(data);
@@ -217,25 +264,12 @@ var removeMessages = function(){
                     }
                 ).done(function () {
                         console.log("done!");
-                        showMessage("Succes to save attribute!", "success");
 
                 }).fail(function(){
                         console.log("fail!");
-                        showMessage("Error to save attribute!", "danger");
                 });
             }
-
-            console.log("attr:"+$scope.attributes.length);
-            console.log("count:"+this.count);
-
-            if($scope.count == $scope.attributes.length){
-                $("#succesToSendData").removeClass();
-                $("#errorToSendData").addClass('hidden');
-            }
-            else{
-                $("#succesToSendData").addClass('hidden');
-                $("#errorToSendData").removeClass();
-            }
+            $scope.attributes = reloadData();
         };
     };
 

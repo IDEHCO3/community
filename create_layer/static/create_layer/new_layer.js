c4d375af607ff9b0fe39179c76d3bf7b3db16eef
Kaitsuru <aluizio@dcc.ufrj.br>
2015-08-04 20:23:58 -0300

    Refactored create layer.

--- create_layer/static/create_layer/new_layer.js
+++ create_layer/static/create_layer/new_layer.js
@@ -53,10 +53,45 @@ var convertToNG = function(attr,i){
     return newAttr;
 };
 
-var globalData = [];
+var convertType = function(type, decimal){
+    switch(type){
+        case 'Character':
+            type = "CharField";
+            break;
+        case 'Number':
+            type = "DecimalField";
+            break;
+        case 'Logical':
+            type = "BooleanField";
+            break;
+        case 'Date':
+            type = "DateTimeField";
+            break;
+        default:
+            type = "CharField";
+            break;
+    }
+
+    if(type == "DecimalField" && decimal == 0) {
+        return "IntegerField";
+    }
+
+    return type;
+};
+
+var convertToRestFul = function(attribute){
+    var type = convertType(attribute.attributeType, attribute.attributeDecimal);
+    return {
+        "name_field": attribute.attributeName,
+        "type_field": type,
+        "name_module_field": "django.forms",
+        "options": "{\"max_length\": \""+attribute.attributeSize+"\", \"blank\": \"false\"}",
+        "community": community_id
+    };
+};
 
 var loadData = function(dataIncoming){
-    data = [];
+    var data = [];
     for(var i=0; i<dataIncoming.length; i++){
         var attr = dataIncoming[i];
         var newAttr = convertToNG(attr,i);
@@ -64,23 +99,26 @@ var loadData = function(dataIncoming){
         data.push(newAttr);
     }
 
-    globalData = data;
-};
-
-var reloadData = function(){
-    $.get(url_filter).done(loadData);
-    return globalData;
+    return data;
 };
 
 (function(){
-    var app = angular.module("layer_app",[]);
-    var data = [];
+    var app = angular.module("layer_app",[]).config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+    }).run(run);
 
-    app.layerController = function($scope){
+    run.$inject = ['$http'];
+    function run($http){
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
+
+    app.layerController = function($scope, $http){
 
         $scope.layerName = 'myLayer';
         $scope.layerType = 'Point';
-        $scope.attributes = reloadData();
+        $scope.attributes = [];
 
         $scope.attributeName = 'attributeName';
         $scope.attributeType = 'Character';
@@ -88,6 +126,10 @@ var reloadData = function(){
         $scope.attributeDecimal = 0;
 
         $scope.deletedList = [];
+
+        $http.get(url_filter).success(function(dataIncoming){
+            $scope.attributes = loadData(dataIncoming);
+        });
 
         this.setAttributeType = function(type){
             $scope.attributeType = type;
@@ -113,62 +155,6 @@ var reloadData = function(){
             else{
                 return false;
             }
-        };
-
-        var convertType = function(type, decimal){
-            switch(type){
-                case 'Character':
-                    type = "CharField";
-                    break;
-                case 'Number':
-                    type = "DecimalField";
-                    break;
-                case 'Logical':
-                    type = "BooleanField";
-                    break;
-                case 'Date':
-                    type = "DateTimeField";
-                    break;
-                default:
-                    type = "CharField";
-                    break;
-            }
-
-            if(type == "DecimalField" && decimal == 0) {
-                return "IntegerField";
-            }
-
-            return type;
-        };
-
-        var convertToRestFul = function(attribute){
-            var type = convertType(attribute['attributeType'], attribute['attributeDecimal']);
-            return {
-                "id": 1,
-                "name_field": attribute['attributeName'],
-                "type_field": type,
-                "name_module_field": "django.forms",
-                "options": "{\"max_length\": \""+attribute['attributeSize']+"\", \"blank\": \"false\"}",
-                "community": community_id
-            };
-        };
-
-        var saveAttributeOnDB = function(attr){
-            removeMessages();
-            var localData = convertToRestFul(attr);
-            var dataJson = JSON.stringify(localData);
-
-            $.post(url_create,
-                {
-                    _content_type: "application/json",
-                    _content: dataJson
-                }
-            ).done(function (dataIncoming) {
-                    showMessage("Succes to save attribute!", "success");
-
-            }).fail(function(){
-                    showMessage("Error to save attribute!", "danger");
-            });
         };
 
         this.createAttribute = function(){
@@ -198,16 +184,14 @@ var reloadData = function(){
             if( $scope.attributeDecimal > 255 || $scope.attributeDecimal < 0 )
                 return;
 
-            var attr = {'id': $scope.attributes.length,
-                        'pk': null,
-                        'attributeName': $scope.attributeName,
-                        'attributeType':  $scope.attributeType,
-                        'attributeSize': $scope.attributeSize,
-                        'attributeDecimal': $scope.attributeDecimal };
+            var attr = {id: $scope.attributes.length,
+                        pk: null,
+                        attributeName: $scope.attributeName,
+                        attributeType:  $scope.attributeType,
+                        attributeSize: $scope.attributeSize,
+                        attributeDecimal: $scope.attributeDecimal };
 
-            //saveAttributeOnDB(attr);
             $scope.attributes.push(attr);
-            //reloadData();
 
             $scope.attributeName = 'attributeName';
             $scope.attributeType = 'Character';
@@ -215,23 +199,10 @@ var reloadData = function(){
             $scope.attributeDecimal = 0;
         };
 
-        var deleteAttributesFromDB = function(){
-            for(var i=0; i<$scope.deletedList.length; i++) {
-                var pk = $scope.deletedList[i];
-                var url_delete = url_update + pk + "/";
-                $.ajax({
-                    url: url_delete,
-                    method: "DELETE"
-                });
-            }
-        };
-
         $scope.removeAttribute = function(index){
             var id = $scope.attributes[index]['pk'];
             if( id != null ) $scope.deletedList.push(id);
 
-            //deleteAttributeFromDB(index);
-            //reloadData();
             $scope.attributes.splice(index,1);
 
             for(var i=0; i < $scope.attributes.length; i++){
@@ -247,97 +218,53 @@ var reloadData = function(){
             this.removeAttribute(index);
         };
 
-        this.sendData = function(){
+        var deleteAttributesFromDB = function(){
+            for(var i=0; i<$scope.deletedList.length; i++) {
+                var pk = $scope.deletedList[i];
+                var url_delete = url_update + pk + "/";
+                $http.delete(url_delete);
+            }
+        };
+
+        var findNullPK = function(){
+            var x = -1;
+            for(var i=0; i<$scope.attributes.length; i++){
+                if($scope.attributes[i].pk == null){
+                    x = i;
+                    break;
+                }
+            }
+            return x;
+        };
+
+        $scope.sendData = function(){
             removeMessages();
             deleteAttributesFromDB();
 
             for(var i=0; i<$scope.attributes.length; i++) {
-                if($scope.attributes[i]['pk'] != null) continue;
+                if($scope.attributes[i].pk != null) continue;
 
-                var data = convertToRestFul($scope.attributes[i]);
-                var dataJson = JSON.stringify(data);
+                var attr = $scope.attributes[i];
+                var localData = convertToRestFul(attr);
 
-                $.post(url_create,
+                $http.post(url_create, localData,
                     {
-                        _content_type: "application/json",
-                        _content: dataJson
-                    }
-                ).done(function () {
-                        console.log("done!");
+                        headers: {'Content-Type': "application/json"}
+                    })
+                    .success(function (dataIncoming) {
+                        var x = findNullPK();
+                        $scope.attributes[x] = convertToNG(dataIncoming, x);
+                        showMessage("Succes to save attribute: '"+attr.attributeName+"'!", "success");
 
-                }).fail(function(){
-                        console.log("fail!");
-                });
+                    })
+                    .error(function(){
+                        showMessage("Error to save attribute: '"+attr.attributeName+"'!", "danger");
+                    }
+                );
             }
-            $scope.attributes = reloadData();
         };
     };
 
-    app.controller("LayerController",['$scope', app.layerController]);
-
-    app.directive("attributesList", function(){
-        return {
-            restrict: 'E',
-            templateUrl: '/static/create_layer/attributes-list.html'
-        };
-    });
-
-    app.directive("layerInput", function(){
-        return {
-            restrict: 'E',
-            templateUrl: '/static/create_layer/layer-input.html',
-            controller: 'LayerController',
-            controllerAs: 'layer'
-        };
-    });
-
-
+    app.controller("LayerController",['$scope', '$http', app.layerController]);
 
 })();
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-var csrftoken = getCookie('csrftoken');
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-function sameOrigin(url) {
-    // test that a given url is a same-origin URL
-    // url could be relative or scheme relative or absolute
-    var host = document.location.host; // host + port
-    var protocol = document.location.protocol;
-    var sr_origin = '//' + host;
-    var origin = protocol + sr_origin;
-    // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-        // or any other URL that isn't scheme relative or absolute i.e relative.
-        !(/^(\/\/|http:|https:).*/.test(url));
-}
-
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});

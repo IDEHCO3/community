379b863065eaad3a9850096e429644a9f9be6078
aluizio <aluizio@dcc.ufrj.br>
2015-08-20 19:34:30 -0300

    Created the list of bookmarks of communities.

---
+++ community/static/community/community_controller.js
@@ -0,0 +1,23 @@
+(function(){
+    var app = angular.module("community_app",[]).config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+    });
+
+    /*app.run(['$http', function($http) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('login' + ':' + 'password');
+    }]);*/
+
+    app.controller("BookmarkerController",['$http','$scope', function($http, $scope){
+        $scope.bookmarks = [];
+
+        $http.get("http://127.0.0.1:8000/bookmarks/")
+            .success(function(data){
+                data.forEach(function(d){
+                    if(d.resourceType == "communities") $scope.bookmarks.push(d);
+                });
+            }).error(function(data){
+                console.log("Error to load data: "+ data.detail);
+            });
+    }]);
+})();

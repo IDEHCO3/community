c224321979b3681110610179e78e07a418965bd0
aluizio <aluizio@dcc.ufrj.br>
2015-08-20 20:53:43 -0300

    load bookmark to community works.

--- community/static/community/community_controller.js
+++ community/static/community/community_controller.js
@@ -14,7 +14,12 @@
         $http.get("http://127.0.0.1:8000/bookmarks/")
             .success(function(data){
                 data.forEach(function(d){
-                    if(d.resourceType == "communities") $scope.bookmarks.push(d);
+                    if(d.resourceType == "communities") {
+                        var coord = JSON.parse(d.coordinates);
+                        d.lat = coord[0];
+                        d.lng = coord[1];
+                        $scope.bookmarks.push(d);
+                    }
                 });
             }).error(function(data){
                 console.log("Error to load data: "+ data.detail);

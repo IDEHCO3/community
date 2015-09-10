343ffea7906cd0e022136281dcf6fb78e1de861b
aluizio <aluizio@dcc.ufrj.br>
2015-09-10 13:07:56 -0300

    Moved community_app detail to community detail.

--- community/views.py
+++ community/views.py
@@ -12,6 +12,10 @@ from permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from community.serializers import CommunitySerializer
 
+from community_app.forms import FactoryForm
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 
 #List communities
@@ -77,3 +81,50 @@ class InviteSomeone():
     recipients = ['info@example.com']
     """
    # send_mail("subject", "message", "sender", ["recipients"])
+
+def as_json_str(communityinformationfieldschema_set):
+    print(communityinformationfieldschema_set)
+    a_list = list(('\"' + cif + '\"' + ':' + '\"\"') for cif in communityinformationfieldschema_set)
+    return "{" + (",".join(a_list)) + "}"
+
+def community_detail(request, pk, lat=0, lng=0, zoom=0):
+    community = Community.objects.get(pk=pk)
+
+    cifs = community.communityinformationfieldschema_set.all()
+    a_list = list(cif.name_field for cif in cifs)
+
+    schema_field = str(a_list).replace("'", '"')
+    schema_field = schema_field.replace('u"', '"')
+
+    a_form = FactoryForm.create(cifs)
+    schema_json_str = as_json_str(a_list)
+
+    geometry_type = ""
+    for type in cifs:
+        if type.name_field == "geometry":
+            geometry_type = str(type.type_field)
+            break
+
+    url_list = "/community_app/"+str(pk)+"/community_information_list/?format=json"
+    url_create = "/community_app/"+str(pk)+"/community_information_create/"
+    url_update = "/community_app/community_information_detail/"
+    url_community = "/communities/"+str(pk)+"/"
+
+    context = {
+        "request": request,
+        "community": community,
+        "zoom": zoom,
+        "lat": lat,
+        "lng": lng,
+        "geometry_type": geometry_type,
+        "url_community": url_community,
+        "url_json": url_list,
+        "form": a_form,
+        "schema_field": schema_field,
+        "schema_json_str": schema_json_str,
+        "url_create": url_create,
+        "url_update": url_update
+    }
+
+    return render_to_response('community/detail/index.html',
+                              RequestContext(request, context))
--- community_app/urls.py
+++ community_app/urls.py
@@ -5,8 +5,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     #url(r'^list/$', views.community_information_list, name='community_information_list'),
-    url(r'^detail/(?P<pk>\d+)/$', views.community_detail, name='community_detail'),
-    url(r'^detail/(?P<pk>\d+)/(?P<lat>\-?\d+\.?\d*),(?P<lng>\-?\d+\.?\d*),(?P<zoom>\d+)$', views.community_detail, name='community_detail_coordinates'),
     url(r'^community_information_detail/(?P<pk>\d+)$', views.community_information_detail, name='community_information_detail'),
     url(r'^(?P<pk>\d+)/community_information_list/$', views.community_information_list, name='community_information_list'),
     url(r'^(?P<pk>\d+)/community_information_create/$', views.community_information_create, name='community_information_create'),

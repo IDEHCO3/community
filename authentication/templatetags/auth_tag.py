6415b4d460a5034fb9d33533c435d182b6394c54
Aluizio <aluizio777@gmail.com>
2015-09-14 18:10:22 -0300

    Now the authentication page can show login fail.

--- authentication/templatetags/auth_tag.py
+++ authentication/templatetags/auth_tag.py
@@ -4,9 +4,9 @@ register = template.Library()
 
 @register.inclusion_tag("authentication/auth_login.html")
 def login(redirection_url):
-    return {'destiny': redirection_url}
+    return {'next': redirection_url}
 
 @register.inclusion_tag("authentication/auth_logout.html", takes_context=True)
 def logout(context, redirection_url):
-    context['destiny'] = redirection_url
+    context['next'] = redirection_url
     return context

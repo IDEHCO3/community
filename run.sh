e20a9973990ec47dd0ea3da840f8cab4dc5194b7
aluizio <aluizio@dcc.ufrj.br>
2016-02-17 11:43:00 -0200

    Fixed a bug in run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ET="eth0"
-if [ "$1" == "" ]; then
+if [ "$1" != "" ]; then
     ET=$1
 fi
 
-IP="$(ifconfig enp2s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
+IP=""
+IP="$(ifconfig $ET | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 if [ "$IP" != "" ] ; then
     python manage.py runserver $IP:8000
 fi
